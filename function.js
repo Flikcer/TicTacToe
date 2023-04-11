96ac28d554fb81ed1a2c24266d9d22a88751ef48
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-11 17:27:43 -0400

    fully functional JS V2

--- function.js
+++ function.js
@@ -1,225 +1,124 @@
-// Set initial game state variables
+//set to true so game will start with playerclick
 var playerTurn = true;
 var computerMoveTimeout = 0;
 var usedIndexes = 0;
-var gameBoardTable = document.getElementById("gameBoard");
-var turnInfo = document.getElementById("turnInfo");
 
-// Attach click event listener to game board
-gameBoardTable.addEventListener("click", cellClicked);
+//possible winning combinations
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-// Attach click event listener to "New Game" button
-document.getElementById("newGameButton").addEventListener("click", newGame);
+//load dom
+window.addEventListener("DOMContentLoaded", domLoaded);
 
-// Function to start a new game
+//get cells in table
+function domLoaded() {
+  let newBtn = document.getElementById("newGameButton");
+  newBtn.addEventListener("click", newGame);
+  let cells = getGameBoard();
+  for (let cell of cells) {
+    cell.addEventListener("click", function () {
+      cellClicked(this);
+    });
+  }
+
+  newGame();
+}
+
+//return the array of the game board to correspond with combinations and display
+function getGameBoard() {
+  return Array.from(document.querySelectorAll("#gameBoard td"));
+}
+
+//clear and reset to new game
 function newGame() {
-  // Reset game state variables
   clearTimeout(computerMoveTimeout);
   computerMoveTimeout = 0;
   usedIndexes = 0;
-
-  // Clear the game board
-  var spaces = gameBoardTable.getElementsByTagName("td");
-  for (var i = 0; i < spaces.length; i++) {
-    spaces[i].innerHTML = "&nbsp;";
-  }
-
-  // Set the starting player and turn information
+  var spaces = getGameBoard();
+  spaces.forEach((space) => (space.textContent = ""));
   playerTurn = true;
-  turnInfo.textContent = "Your turn";
+  document.getElementById("turnInfo").textContent = "Your turn";
 }
 
-// Function to handle a click on a game board cell
-function cellClicked(event) {
-  var target = event.target;
-
-  // Check that the clicked element is a game board cell, it's the player's turn,
-  // and the cell hasn't already been filled
-  if (target.tagName !== "TD" || !playerTurn || target.textContent !== "\u00a0") {
-    return;
+//mark with red x for player click
+function cellClicked(cell) {
+  if (playerTurn && cellIsEmpty(cell)) {
+    markCell(cell, "X", "red");
+    usedIndexes++;
+    switchTurn();
   }
-
-  // Fill the cell with an "X" and switch to the next turn
-  target.textContent = "X";
-  target.style.color = "red";
-  usedIndexes += 1;
-  switchTurn();
 }
 
-// Function to switch to the next turn
+//display for empty cell
+function cellIsEmpty(cell) {
+  return cell.textContent.trim() === "";
+}
+
+//function call to mark cell with param of color letter and where
+function markCell(cell, symbol, color) {
+  cell.textContent = symbol;
+  cell.style.color = color;
+}
+
+//if not playerturn call computer move after 1 second
 function switchTurn() {
-  var board = gameBoardTable.getElementsByTagName("td");
-  var draw = true;
-  var winLookup = {
-    "012": "X",
-    "345": "X",
-    "678": "X",
-    "036": "X",
-    "147": "X",
-    "258": "X",
-    "048": "X",
-    "246": "X",
-    "012": "O",
-    "345": "O",
-    "678": "O",
-    "036": "O",
-    "147": "O",
-    "258": "O",
-    "048": "O",
-    "246": "O",
-  };
-
-  // Check if the player or computer has won the game
-  for (var key in winLookup) {
-    if (
-      board[key[0]].textContent === winLookup[key] &&
-      board[key[1]].textContent === winLookup[key] &&
-      board[key[2]].textContent === winLookup[key]
-    ) {
-      playerTurn = false;
-      gameOver = true;
-      turnInfo.textContent =
-        winLookup[key] === "X" ? "You win!" : "Computer wins!";
-      return;
-    }
-  }
-
-  // Check if the game is a draw
-  for (var i = 0; i < board.length; i++) {
-    if (board[i].textContent === "\u00a0") {
-      draw = false;
-      break;
-    }
-  }
-
-  if (draw) {
-    playerTurn = false;
-    gameOver = true;
-    turnInfo.textContent = "Draw!";
+  if (checkForWin()) {
     return;
   }
-
-  // If the game is still in progress, switch to the next player's turn
   if (playerTurn) {
     playerTurn = false;
-    turnInfo.textContent = "Computer's turn";
+    document.getElementById("turnInfo").textContent = "Computer's turn";
     computerMoveTimeout = setTimeout(makeComputerMove, 1000);
   } else {
     playerTurn = true;
-    turnInfo.textContent = "Your turn";
+    document.getElementById("turnInfo").textContent = "Your turn";
   }
-
-
-
-// Set initial game state variables
-var playerTurn = true;
-var computerMoveTimeout = 0;
-var usedIndexes = 0;
-var gameBoardTable = document.getElementById("gameBoard");
-var turnInfo = document.getElementById("turnInfo");
-
-// Attach click event listener to game board
-gameBoardTable.addEventListener("click", cellClicked);
-
-// Attach click event listener to "New Game" button
-document.getElementById("newGameButton").addEventListener("click", newGame);
-
-// Function to start a new game
-function newGame() {
-  // Reset game state variables
-  clearTimeout(computerMoveTimeout);
-  computerMoveTimeout = 0;
-  usedIndexes = 0;
-
-  // Clear the game board
-  var spaces = gameBoardTable.getElementsByTagName("td");
-  for (var i = 0; i < spaces.length; i++) {
-    spaces[i].innerHTML = "&nbsp;";
-  }
-
-  // Set the starting player and turn information
-  playerTurn = true;
-  turnInfo.textContent = "Your turn";
 }
 
-// Function to handle a click on a game board cell
-function cellClicked(event) {
-  var target = event.target;
-
-  // Check that the clicked element is a game board cell, it's the player's turn,
-  // and the cell hasn't already been filled
-  if (target.tagName !== "TD" || !playerTurn || target.textContent !== "\u00a0") {
-    return;
+//check winning combinations array and then display winner, if 9 boxes full display tie
+function checkForWin() {
+  const board = getGameBoard();
+  for (let i = 0; i < winCombinations.length; i++) {
+    const [a, b, c] = winCombinations[i];
+    if (
+      board[a].textContent === board[b].textContent &&
+      board[b].textContent === board[c].textContent &&
+      !cellIsEmpty(board[a])
+    ) {
+      endGame(board[a].textContent === "X" ? "You win!" : "Computer wins!");
+      return true;
+    }
   }
-
-  // Fill the cell with an "X" and switch to the next turn
-  target.textContent = "X";
-  target.style.color = "red";
-  usedIndexes += 1;
-  switchTurn();
+  if (usedIndexes === 9) {
+    endGame("It's a tie!");
+    return true;
+  }
+  return false;
 }
 
-// Function to switch to the next turn
-function switchTurn() {
-  var board = gameBoardTable.getElementsByTagName("td");
-  var draw = true;
-  var winLookup = {
-    "0": ["012", "036", "048"],
-    "1": ["012", "147"],
-    "2": ["012", "258", "246"],
-    "3": ["345", "036"],
-    "4": ["345", "147", "258", "048"],
-    "5": ["345", "258"],
-    "6": ["678", "036", "246"],
-    "7": ["678", "147"],
-    "8": ["678", "258", "048"]
-  };
+//message of who won to be called above as callback
+function endGame(message) {
+  playerTurn = false;
+  document.getElementById("turnInfo").textContent = message;
+}
 
-  // Check if the player or computer has won the game
-  for (var key in winLookup) {
-    if (
-      board[key].textContent === "X" &&
-      board[winLookup[key][0]].textContent === "X" &&
-      board[winLookup[key][1]].textContent === "X"
-    ) {
-      playerTurn = false;
-      gameOver = true;
-      turnInfo.textContent = "You win!";
-      return;
-    }
-    if (
-      board[key].textContent === "O" &&
-      board[winLookup[key][0]].textContent === "O" &&
-      board[winLookup[key][1]].textContent === "O"
-    ) {
-      playerTurn = false;
-      gameOver = true;
-      turnInfo.textContent = "Computer wins!";
-      return;
-    }
+//random computer move
+function makeComputerMove() {
+  if (!playerTurn) {
+    const board = getGameBoard();
+    const emptyCells = board.filter(cellIsEmpty);
+    const randomCell =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    markCell(randomCell, "O", "black");
+    usedIndexes++;
+    switchTurn();
   }
-
-  // Check if the game is a draw
-  for (var i = 0; i < board.length; i++) {
-    if (board[i].textContent === "\u00a0") {
-      draw = false;
-      break;
-    }
-  }
-
-  if (draw) {
-    playerTurn = false;
-    gameOver = true;
-    turnInfo.textContent = "Draw!";
-    return;
-  }
-
-  // If the game is still in progress, switch to the next player's turn
-  if (playerTurn) {
-    playerTurn = false;
-    turnInfo.textContent = "Computer's turn";
-    computerMoveTimeout = setTimeout(makeComputerMove, 1000);
-  } else {
-    playerTurn = true;
-    turnInfo.textContent = "Your turn";
-  }
+}
